98b76b576164177c400044dddea35125955db0e8
OpenAvril <Jasper.Assembly@icloud.com>
2026-07-28 13:33:28 +1000

    DEVELOPMENT_2026_08_28_1332, <COMPILE_SUCCESS> <RUNTIME_TEST>.
    IMPLEMENTED:
    - flag once thread under condiseration is relased set to control dynamic stagger.

--- SUBJECT_A__Computer_Systems/Lesson_2__CLIB_The_DLL__Write_Que_For_Thread_At_Registers/src/WriteEnableForThreadsAt_STACK__Global.cpp
+++ SUBJECT_A__Computer_Systems/Lesson_2__CLIB_The_DLL__Write_Que_For_Thread_At_Registers/src/WriteEnableForThreadsAt_STACK__Global.cpp
@@ -1,13 +1,12 @@
 #include "../include/WriteEnableForThreadsAt_STACK__Global.h"
-
 #include <climits>
 #include <cstdint>
 #include <cstring>
 #include <iostream>
-std::array<bool,2>* _stat_REG_CONST_WriteEnableForThreadsAt_STACK__2bitFLAG_IDLE;
-std::array<bool,2>* _stat_REG_CONST_WriteEnableForThreadsAt_STACK__2bitFLAG_WAIT;
-std::array<bool,2>* _stat_REG_CONST_WriteEnableForThreadsAt_STACK__2bitFLAG_WRITE;
-uint8_t* _stat_REG_ptr_WriteEnableForThreadsAt_STACK__number_Of_Implemented_Threads;
+std::array<bool,2>* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::_stat_REG_CONST_WriteEnableForThreadsAt_STACK__2bitFLAG_IDLE;
+std::array<bool,2>* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::_stat_REG_CONST_WriteEnableForThreadsAt_STACK__2bitFLAG_WAIT;
+std::array<bool,2>* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::_stat_REG_CONST_WriteEnableForThreadsAt_STACK__2bitFLAG_WRITE;
+uint8_t* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::_stat_REG_ptr_WriteEnableForThreadsAt_STACK__number_Of_Implemented_Threads;
 CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::WriteEnableForThreadsAt_STACK__Global()
 {
 
@@ -70,6 +69,15 @@ uint8_t CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::dyn_REG_get_
 int CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__Bool_To_Int(bool value)
 {
     return pr_stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__Bool_To_Int(value);
+}
+unsigned char* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__MsbFloat_To_MsbByteArray(float value) {
+    return pr_stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__MsbFloat_To_MsbByteArray(value);
+}
+unsigned char* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__MsbDouble_To_MsbByteArray(double value) {
+    return pr_stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__MsbDouble_To_MsbByteArray(value);
+}
+unsigned char* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__Msb_uint8_t_to_MsbByteArray(uint8_t value) {
+    return pr_stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__Msb_uint8_t_to_MsbByteArray(value);
 }
 unsigned char* CLIBWriteQueAtSTACK::WriteEnableForThreadsAt_STACK__Global::stat_APP_CONVERT_WriteEnableForThreadsAt_STACK__MsbBoolean_To_MsbByteArray(bool value)
 {
